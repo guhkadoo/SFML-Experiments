0628585828d8cd3e597dc03d769dd371289bfc6a
guhkadoo <gustavokadoo@hotmail.com>
2024-09-19 10:59:24 -0300

    created class TextureHolder

--- moving_ball/sfml_test.cpp
+++ moving_ball/sfml_test.cpp
@@ -1,4 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <memory>
+#include <assert.h>
+
+namespace Textures
+{
+    enum ID { Landscape, Airplane, Missile };
+}
+
+class TextureHolder
+{
+    private:
+        std::map<Textures::ID, std::unique_ptr<sf::Texture>> mTextureMap;
+    public:
+        void load(Textures::ID id, const std::string& filename);
+        sf::Texture& get(Textures::ID id);
+        const sf::Texture& get(Textures::ID id) const;
+
+};
+
+void TextureHolder::load(Textures::ID id, const std::string& filename)
+{
+    std::unique_ptr<sf::Texture> texture(new sf::Texture());
+    if(!texture->loadFromFile(filename))
+        throw std::runtime_error("TextureHolder::load - Failed to load " + filename);
+
+    auto inserted = mTextureMap.insert(std::make_pair(id, std::move(texture)));
+    assert(inserted.second);
+};
+
+sf::Texture& TextureHolder::get(Textures::ID id)
+{
+    auto found = mTextureMap.find(id); 
+    assert(found != mTextureMap.end());
+
+    return *found->second;
+}
+
+const sf::Texture& TextureHolder::get(Textures::ID id) const
+{
+    auto found = mTextureMap.find(id);
+    assert(found != mTextureMap.end());
+
+    return *found->second;
+}
 
 class Game
 {
